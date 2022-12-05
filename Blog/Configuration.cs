520e484b4f16611984a7efa74dcd7a963c9344d2
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-12-05 15:36:34 -0300

    ApiKey

--- Blog/Configuration.cs
+++ Blog/Configuration.cs
@@ -4,4 +4,6 @@ public static class Configuration
 {
     // TOKEN - Jason Web Token
     public static string JwtKey {get; set;} = "ab09064f8d8d4a8ca870c5fd44a8737c=";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_api_IlTevUM/Z0ey3nwCV/unWg==";
 }
