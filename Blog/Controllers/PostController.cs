05e315a206d15912a25c932105119978c4153043
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-12-20 13:57:46 -0300

    Paginando Dados

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -14,28 +14,41 @@ public class PostController: ControllerBase
 {
     [HttpGet("v1/posts")]
     public async Task<IActionResult> GetAsync(
-        [FromServices] BlogDataContext context
+        [FromServices] BlogDataContext context,
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 25
     )
     {
         try
         {
+            var count = await context.Posts.AsNoTracking().CountAsync();
             var posts = await context
             .Posts
             .AsNoTracking()
             .Include(x => x.Category)
             .Include(x => x.Author)
-            // .Select(x => new ListPostViewModel
-            // {
-            //     Id = x.Id,
-            //     Title = x.Title,
-            //     Slug = x.Slug,
-            //     LastUpdateDate = x.LastUpdateDate,
-            //     Category = x.Category.Name,
-            //     Author = $"{x.Author.Name} {x.Author.Email}"
-            // }
-            // )
+            .Select(x => new ListPostViewModel
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Slug = x.Slug,
+                LastUpdateDate = x.LastUpdateDate,
+                Category = x.Category.Name,
+                Author = $"{x.Author.Name} {x.Author.Email}"
+            }
+            )
+            .Skip(page * pageSize)
+            .Take(pageSize)
+            .OrderByDescending(x => x.LastUpdateDate)
             .ToListAsync();
-            return Ok(posts);
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                total = count,
+                page,
+                pageSize,
+                posts
+            }));
         }
         catch 
         {
